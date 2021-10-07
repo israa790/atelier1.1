eae2cc5eb2c7628d95f219a1444a3e617c921710
israa790 <80363109+israa790@users.noreply.github.com>
2021-10-07 15:15:31 +0200

    Add files via upload

---
+++ produits/src/main/java/com/isra/produits/service/ProduitService.java
@@ -0,0 +1,20 @@
+package com.isra.produits.service;
+
+import java.util.List;
+
+import com.isra.produits.entities.Produit;
+
+public interface ProduitService {
+	
+Produit saveProduit(Produit p);
+
+Produit updateProduit(Produit p);
+
+void deleteProduit(Produit p);
+
+ void deleteProduitById(Long id);
+ 
+Produit getProduit(Long id);
+
+List<Produit> getAllProduits();
+}
